242a8fef768818c4b5185c9b4d21599a998187e1
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-08-29 15:38:51 +0530

    Added Elastic search base BE code

---
+++ SpringBoot_ElasticSearch_Demo/src/main/java/com/elasticsearch/api/SpringBootElasticSearchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticSearchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElasticSearchDemoApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_ElasticSearch_Demo/src/test/java/com/elasticsearch/api/SpringBootElasticSearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticSearchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
